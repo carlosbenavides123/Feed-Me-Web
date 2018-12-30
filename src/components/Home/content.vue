26130283d18a1a0151d655d8134668f6b8e56be8
carlosbenavides123 <carlos7legend@gmail.com>
2018-12-29 17:48:08 -0800

    pre commit, next step add axios

--- src/components/Home/content.vue
+++ src/components/Home/content.vue
@@ -1,47 +1,52 @@
 <template>
   <v-layout>
     <v-card>
-      <v-container>
-        <v-layout
-          row
-          wrap
-        >
-          <v-flex xs12>
-            <div class="v-text_field">
-              <v-text-field
-                class="mx-2 v-text_field"
-                flat
-                label="Search"
-                prepend-inner-icon="search"
-                solo-inverted
-                placeholder="Search a certain food or leave empty."
-              ></v-text-field>
-            </div>
-          </v-flex>
+      <form @submit.prevent="submitHandler">
+        <v-container>
+          <v-layout
+            row
+            wrap
+          >
+            <v-flex xs12>
+              <div class="v-text_field">
+                <v-text-field
+                  v-model="formdata.query"
+                  class="mx-2 v-text_field"
+                  flat
+                  label="Search"
+                  prepend-inner-icon="search"
+                  solo-inverted
+                  placeholder="Search a certain food or leave empty."
+                ></v-text-field>
+              </div>
+            </v-flex>
 
-          <v-flex xs12>
+            <v-flex xs12>
+              <div>
+                <v-select
+                  v-model="formdata.radius"
+                  :items="radius"
+                  label="Radius"
+                  centered
+                  solo
+                ></v-select>
+              </div>
+            </v-flex>
+
             <div>
-              <v-select
-                :items="radius"
-                label="Radius"
-                centered
-                solo
-              ></v-select>
+              <v-btn
+                type="submit"
+                class="btnprimary"
+                :loading="loading"
+                :disabled="loading"
+                @click="loader = 'loading'"
+                color="primary"
+              >Find me a restaurant!</v-btn>
             </div>
-          </v-flex>
 
-          <div>
-            <v-btn
-              class="btnprimary"
-              :loading="loading"
-              :disabled="loading"
-              @click="loader = 'loading'"
-              color="primary"
-            >Find me a restaurant!</v-btn>
-          </div>
-
-        </v-layout>
-      </v-container>
+          </v-layout>
+        </v-container>
+      </form>
     </v-card>
   </v-layout>
 </template>
@@ -52,7 +57,11 @@ export default {
     return {
       loader: null,
       loading: false,
-      radius: ["Walking Distance", "Drive", "Search around my city"]
+      radius: ["Walking Distance", "Drive", "Search around my city"],
+      formdata: {
+        query: "",
+        radius: ""
+      }
     };
   },
   watch: {
@@ -63,6 +72,11 @@ export default {
       setTimeout(() => (this[l] = false), 3000);
 
       this.loader = null;
+    }
+  },
+  methods: {
+    submitHandler() {
+      this.$store.dispatch("foodsearch/dispatchSearch", this.formdata);
     }
   }
 };
