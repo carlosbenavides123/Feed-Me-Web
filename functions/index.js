56b5f6a93b943f49718da32103b0e4013eb6d5ef
carlosbenavides123 <carlos7legend@gmail.com>
2018-12-30 09:07:06 -0800

    able to call google places api now, need to be able to put parameters next

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
+/*eslint-disable */
 const functions = require('firebase-functions');
+const request = require('request-promise');
+
+/**
+ * Makes a GET request to given URL with the access token
+ */
+function makeApiRequest(url) {
+    return request.get({
+        url: url,
+        transform: body => JSON.parse(body)
+    });
+}
+
+// Our test function
+exports.testApi = functions.https.onRequest((req, res) => {
+    // TODO: process the request, extract parameters, authenticate the user etc
+
+    // The API url to call - edit this
+    const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAEAXEZ8Z-3ReCoSukBYutcDpzCvP9R-Jw`;
+
+    makeApiRequest(url)
+        .then(response => {
+            res.send(response);
+            return;
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
